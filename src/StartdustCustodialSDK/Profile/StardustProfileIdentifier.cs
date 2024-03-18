8035a2bcb5aaced917491ede6e5d0b2a4b1624a3
Youtpout <EddyBoughioul@hotmail.fr>
2024-03-18 21:43:47 +0100

    Add stardust profile identifier

---
+++ src/StartdustCustodialSDK/Profile/StardustProfileIdentifier.cs
@@ -0,0 +1,59 @@
+﻿using StartdustCustodialSDK.Wallet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace StartdustCustodialSDK.Profile
+{
+    public class StardustProfileIdentifier : BaseStardust
+    {
+        public string Id { get; set; }
+        public string RootUserId { get; set; }
+        public string ProfileId { get; set; }
+        public string Service { get; set; }
+        public string Value { get; set; }
+
+        public StardustProfileIdentifier()
+        {
+
+        }
+
+        public StardustProfileIdentifier(string id, string rootUserId, string profileId, string service, string value)
+        {
+            Id = id;
+            RootUserId = rootUserId;
+            ProfileId = profileId;
+            Service = service;
+            Value = value;
+        }
+
+        // TODO: implement opinionated validation for each service type
+        public static bool ValidateIdentifier(StardustProfileIdentifierService service, string value)
+        {
+            switch (service)
+            {
+                case StardustProfileIdentifierService.ExternalWallet:
+                    return true;
+                case StardustProfileIdentifierService.Discord:
+                    return true;
+                case StardustProfileIdentifierService.Apple:
+                    return true;
+                case StardustProfileIdentifierService.Google:
+                    return true;
+                case StardustProfileIdentifierService.Facebook:
+                    return true;
+                case StardustProfileIdentifierService.Twitter:
+                    return true;
+                case StardustProfileIdentifierService.Email:
+                    return true;
+                case StardustProfileIdentifierService.Phone:
+                    return true;
+
+                default:
+                    return false;
+            }
+        }
+    }
+}
---
+++ src/StartdustCustodialSDK/Profile/StardustProfileIdentifierService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace StartdustCustodialSDK.Profile
+{
+    // Todo verify what type of data come for this enum
+    public enum StardustProfileIdentifierService
+    {
+        [Description("csharp-sdk:external-wallet")]
+        ExternalWallet,
+        [Description("csharp-sdk:discord")]
+        Discord,
+        [Description("csharp-sdk:apple")]
+        Apple,
+        [Description("csharp-sdk:google")]
+        Google,
+        [Description("csharp-sdk:facebook")]
+        Facebook,
+        [Description("csharp-sdk:twitter")]
+        Twitter,
+        [Description("csharp-sdk:email")]
+        Email,
+        [Description("csharp-sdk:phone")]
+        Phone
+    }
+}
