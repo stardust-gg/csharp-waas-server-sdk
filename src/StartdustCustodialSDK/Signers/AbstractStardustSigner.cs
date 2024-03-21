7b7a39f12d03de9b4543c66bf93daba9c6f5423f
Youtpout <EddyBoughioul@hotmail.fr>
2024-03-21 10:09:33 +0100

    add AbstractStardustSigner

---
+++ src/StartdustCustodialSDK/Signers/AbstractStardustSigner.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartdustCustodialSDK.Signers
+{
+    public abstract class AbstractStardustSigner
+    {
+        public abstract Task<string> GetPublicKey();
+        public abstract Task<string> GetAddress();
+        public abstract Task<string> SignRaw<T>(T message);
+    }
+}
---
+++ src/StartdustCustodialSDK/Signers/Nethereum/NethereumStardustSigner.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartdustCustodialSDK.Signers.Nethereum
+{
+    public class NethereumStardustSigner : AbstractStardustSigner
+    {
+        public override Task<string> GetAddress()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<string> GetPublicKey()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<string> SignRaw<T>(T message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
