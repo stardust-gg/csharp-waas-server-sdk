3a29b85452b358a474d98ecf5c1a991f7b8ca777
youtpout <eddyboughioul@hotmail.fr>
2024-04-02 04:15:29 +0200

    add example website with swagger

---
+++ examples/WebsiteExample/WebsiteExample.Server/Controllers/AppController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StartdustCustodialSDK.Application;
+
+namespace WebsiteExample.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppController : ControllerBase
+    {
+        public AppController()
+        {
+          
+        }
+
+        [HttpGet(Name = "GetApp")]
+        public async Task<IActionResult> GetApp([FromQuery]string apiKey)
+        {
+            var stardustApplication = new StardustApplicationAPI(apiKey);
+            var myApp = await stardustApplication.Get();
+            return Ok(myApp);
+        }
+    }
+}
---
+++ examples/WebsiteExample/WebsiteExample.Server/Controllers/ProfileController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StartdustCustodialSDK.Application;
+using StartdustCustodialSDK.Profile;
+
+namespace WebsiteExample.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfileController : ControllerBase
+    {
+
+        [HttpGet(Name = "GetProfile")]
+        public async Task<IActionResult> GetProfile([FromQuery] string apiKey, string profileId)
+        {
+            var stardustProfileApi = new StardustProfileAPI(apiKey);
+            // get it to test if value are the same
+            var getProfile = await stardustProfileApi.Get(profileId);
+            return Ok(getProfile);
+        }
+
+        [HttpPost(Name = "AddProfile")]
+        public async Task<IActionResult> AddProfile(ProfileData profileData)
+        {
+
+            var stardustApplication = new StardustApplicationAPI(profileData.ApiKey);
+            var myApp = await stardustApplication.Get();
+
+            var stardustProfileApi = new StardustProfileAPI(profileData.ApiKey);
+            var profileParam = new StardustProfileCreateParams(myApp.Id, profileData.Name);
+            var profile = await stardustProfileApi.Create(profileParam); 
+            return Ok(profile);
+        }
+    }
+
+    public class ProfileData
+    {
+        public string ApiKey { get; set; }
+        public string Name { get; set; }
+    }
+}
