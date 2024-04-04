a78bfaf3e00b183eebf3eecc188bf75dc59e02d1
Youtpout <EddyBoughioul@hotmail.fr>
2024-04-04 07:14:36 +0200

    Replace webserver example by web api example

--- examples/WebApiExample/Program.cs
+++ examples/WebApiExample/Program.cs
@@ -9,22 +9,19 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-app.UseDefaultFiles();
-app.UseStaticFiles();
-
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My service");
+    c.RoutePrefix = string.Empty;  // Set Swagger UI at apps root
+});
+
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
-
-app.MapFallbackToFile("/index.html");
 
 app.Run();
