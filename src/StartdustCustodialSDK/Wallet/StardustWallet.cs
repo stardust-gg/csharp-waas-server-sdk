66efda7e6df4ff67cf232b58dbec7583aff58d30
Youtpout <EddyBoughioul@hotmail.fr>
2024-04-06 15:24:02 +0200

    add nethereum and evmsigner in wallet

--- src/StartdustCustodialSDK/Wallet/StardustWallet.cs
+++ src/StartdustCustodialSDK/Wallet/StardustWallet.cs
@@ -1,5 +1,7 @@
 ﻿using StartdustCustodialSDK.Application;
 using StartdustCustodialSDK.Profile;
+using StartdustCustodialSDK.Signers.Evm;
+using StartdustCustodialSDK.Signers.Nethereum;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -13,6 +15,11 @@ namespace StartdustCustodialSDK.Wallet
         public string Id { get; set; }
         public string ProfileId { get; set; }
         public StardustApplication Application { get; set; }
+
+        [JsonIgnore]
+        public EvmStardustSigner Evm { get; set; }
+        [JsonIgnore]
+        public NethereumStardustSigner Nethereum { get; set; }
 
         [JsonIgnore]
         public StardustProfileAPI StardustProfileAPI { get; set; }
@@ -29,6 +36,8 @@ namespace StartdustCustodialSDK.Wallet
             Application = application;
             ApiKey = apiKey;
             StardustProfileAPI = new StardustProfileAPI(apiKey);
+            Evm = new EvmStardustSigner(apiKey, id);
+            Nethereum = new NethereumStardustSigner(apiKey, id);
         }
 
         public async Task<StardustProfile> GetProfile()
@@ -41,6 +50,8 @@ namespace StartdustCustodialSDK.Wallet
             base.Init(apiKey);
             this.Application?.Init(apiKey);
             this.StardustProfileAPI = new StardustProfileAPI(apiKey);
+            Evm = new EvmStardustSigner(apiKey, Id);
+            Nethereum = new NethereumStardustSigner(apiKey, Id);
         }
     }
 }
